414648cd7cff8d0b69ee11c26400fd2c31867e97
KyangTan <kwanyang29@gmail.com>
2025-02-14 12:21:09 +0800

    version 12 updated browser tab icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,12 @@ export const metadata: Metadata = {
   title: 'Valentine\'s Day',
   description: 'A Valentine\'s Day gift for my bb',
   generator: 'v0.dev',
+  icons: {
+    icon: '/favicon.png', // /public/favicon.ico
+    // You can also specify different sizes/types:
+    // apple: '/apple-icon.png',
+    // shortcut: '/shortcut-icon.png',
+  },
 }
 
 export default function RootLayout({
